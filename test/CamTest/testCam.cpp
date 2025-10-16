39e20397fb57b376eddf6309496027196da861f9
Seth Jones <sethjonesintern@gmail.com>
2025-10-16 18:47:18 -0400

    new lib deps and code skeleton

--- test/CamTest/testCam.cpp
+++ test/CamTest/testCam.cpp
@@ -1,1 +1,40 @@
 #include <Arducam_Mega.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+
+#define CAM1_PIN 00
+#define CAM2_PIN 00
+#define SD_PIN 00
+
+
+
+Arducam_Mega cam1(CAM1_PIN);
+Arducam_Mega cam2(CAM2_PIN);
+
+
+int initCam(Arducam_Mega &cam){
+
+
+    digitalWrite(CAM1_PIN, HIGH);
+    digitalWrite(CAM2_PIN, HIGH);
+    digitalWrite(SD_PIN, HIGH);
+
+
+
+    if(!cam.begin()){
+        //error starting the camera
+    }
+    cam.f
+    cam.setImageQuality()
+}
+
+
+
+void setup(){
+
+}
+
+void loop(){
+
+}
